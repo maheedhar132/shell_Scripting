da425c76217ec7c6e761fe88b8a89446455766f9
maheedhar132 <maheedhartalluri@gmail.com>
2022-11-03 11:24:30 +0530

    moved the code to respective OS

--- nexus_installation.sh
+++ nexus_installation.sh
@@ -1,33 +1,3 @@
-while getopts ":lu" option;
-do
- case $option in 
- l) echo "Linux OS selected"
-    sudo yum update -y
-    echo "Installing wget"
-    sudo yum install wget -y
-    echo "Installing Java"
-    sudo yum install java-1.8.0-openjdk.x86_64 -y
-    sudo adduser nexus
-    ;;
- u) echo "Ubuntu OS selected"
-    sudo apt update -y
-    echo "Installing Wget"
-    sudo apt install wget -y
-    echo "Installing Java"
-    sudo apt-get install openjdk-8-jdk -y
-    sudo useradd nexus
-    ;;
-esac
-done
-
-sudo mkdir /app && cd /app
-echo "Download latest version of Nexus"
-sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
-echo "Setting up Nexus"
-sudo tar -xvf nexus.tar.gz
-
-
-sudo sed -i 's/run_as_user/run_as_user="nexus"/g' /app/nexus/bin/nexus.rc
 sudo printf "-Xms2703m
 -Xmx2703m
 -XX:MaxDirectMemorySize=2703m
@@ -62,23 +32,62 @@ Restart=on-abort
 [Install]
 WantedBy=multi-user.target" > nexus.service
 
-sudo mv nexus-3* nexus
 
-
-
-sudo mv nexus.service /etc/systemd/system
-sudo mv nexus.vmoptions /app/nexus/bin/nexus.vmoptions
-
-
-
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype-work
-
-do 
+while getopts ":lu" option;
+do
  case $option in 
- l) sudo chkconfig nexus on
+ l) echo "Linux OS selected"
+    sudo yum update -y
+    echo "Installing wget"
+    sudo yum install wget -y
+    echo "Installing Java"
+    sudo yum install java-1.8.0-openjdk.x86_64 -y
+    sudo adduser nexus
+    sudo mkdir /app && cd /app
+    echo "Download latest version of Nexus"
+    sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+    echo "Setting up Nexus"
+    sudo tar -xvf nexus.tar.gz
+    sudo sed -i 's/run_as_user/run_as_user="nexus"/g' /app/nexus/bin/nexus.rc
+
+    sudo mv nexus-3* nexus
+
+
+
+    sudo mv nexus.service /etc/systemd/system
+    sudo mv nexus.vmoptions /app/nexus/bin/nexus.vmoptions
+
+
+
+    sudo chown -R nexus:nexus /app/nexus
+    sudo chown -R nexus:nexus /app/sonatype-work
+    sudo chkconfig nexus on
     ;;
- u) update-rc.d nexus on
+ u) echo "Ubuntu OS selected"
+    sudo apt update -y
+    echo "Installing Wget"
+    sudo apt install wget -y
+    echo "Installing Java"
+    sudo apt-get install openjdk-8-jdk -y
+    sudo useradd nexus
+    sudo mkdir /app && cd /app
+    echo "Download latest version of Nexus"
+    sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+    echo "Setting up Nexus"
+    sudo tar -xvf nexus.tar.gzsudo sed -i 's/run_as_user/run_as_user="nexus"/g' /app/nexus/bin/nexus.rc
+
+    sudo mv nexus-3* nexus
+
+
+
+    sudo mv nexus.service /etc/systemd/system
+    sudo mv nexus.vmoptions /app/nexus/bin/nexus.vmoptions
+
+
+
+    sudo chown -R nexus:nexus /app/nexus
+    sudo chown -R nexus:nexus /app/sonatype-work
+    update-rc.d nexus on
     ;;
 esac
 done
